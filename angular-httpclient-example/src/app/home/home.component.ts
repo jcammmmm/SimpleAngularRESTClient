4fd93c2a568585ec9d311fc5e5e1151373b5e101
Camilo Florez <jcflorezv@unal.edu.co>
2020-02-25 20:42:14 -0600

    GET all list action and drawing on client

--- angular-httpclient-example/src/app/home/home.component.ts
+++ angular-httpclient-example/src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    products = []; 
+    
+    constructor(private dataService: DataService) { } 
+    
+    ngOnInit() {
+      this.dataService.sendGetRequest().subscribe((data: any[]) => {
+        console.log(data);
+        this.products = data;
+      })
+    }
 }
