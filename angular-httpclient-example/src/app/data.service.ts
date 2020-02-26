08a14890b5660b6f45634ef19277665f749dd8ff
Camilo Florez <jcflorezv@unal.edu.co>
2020-02-25 21:11:15 -0600

    client or server error handling

--- angular-httpclient-example/src/app/data.service.ts
+++ angular-httpclient-example/src/app/data.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient, HttpErrorResponse } from "@angular/common/http"; 
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class DataService {
-    private REST_API_SERVER = "http://localhost:3000"; 
+    private REST_API_SERVER = "http://localhost:3000/products"; 
     
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) { } 
+    
+    handleError(error: HttpErrorResponse) {
+      let errorMessage = 'Unknown error!';
+      if (error.error instanceof ErrorEvent) {
+        // Client-side errors
+        errorMessage = `Error: ${error.error.message}`;
+      } else {
+        // Server-side errors
+        errorMessage = `Error Code: ${error.status}\n Message: ${error.message}`;
+      }
 
-    public sendGetRequest(){
-      return this.httpClient.get(this.REST_API_SERVER);
+      window.alert(errorMessage);
+      return throwError(errorMessage);
+      
+    } public sendGetRequest() {
+      return this.httpClient.get(this.REST_API_SERVER).pipe(catchError(this.handleError));
     }
 }
